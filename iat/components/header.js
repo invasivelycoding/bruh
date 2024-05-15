ce9ddde27ba9a9eaa27b96069a13a3941a564b56
71ne <45244555+invasivelycoding@users.noreply.github.com>
2024-05-15 14:24:21 -0500

    jkjkj

--- iat/components/header.js
+++ iat/components/header.js
@@ -36,50 +36,9 @@ class Header extends React.Component {
               `fas`,
               this.state.showSidebar? `fa-times` : `fa-bars`
             )}
-          />
-        </div>
-        <nav className="navbar navbar-expand-md navbar-light">
-          <div className="container">
-            <div className="row">
-              <div className="col-12">
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                  <ul className="navbar-nav ml-auto py-4 py-md-0">
-                    <li className="nav-item pl-4 pl-md-0 ml-0 ml-md-4 active">
-                      <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Home</a>
-                      <div className="dropdown-menu">
-                        <a className="dropdown-item" href="#">Action</a>
-                        <a className="dropdown-item" href="#">Another action</a>
-                        <a className="dropdown-item" href="#">Something else here</a>
-                        <a className="dropdown-item" href="#">Another action</a>
-                      </div>
-                    </li>
-                    <li className="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
-                      <a className="nav-link" href="#">Portfolio</a>
-                    </li>
-                    <li className="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
-                      <a className="nav-link" href="#">Agency</a>
-                    </li>
-                    <li className="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
-                      <a className="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Services</a>
-                      <div className="dropdown-menu">
-                        <a className="dropdown-item" href="#">Action</a>
-                        <a className="dropdown-item" href="#">Another action</a>
-                        <a className="dropdown-item" href="#">Something else here</a>
-                        <a className="dropdown-item" href="#">Another action</a>
-                      </div>
-                    </li>
-                    <li className="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
-                      <a className="nav-link" href="#">Journal</a>
-                    </li>
-                    <li className="nav-item pl-4 pl-md-0 ml-0 ml-md-4">
-                      <a className="nav-link" href="#">Contact</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+            />
             </div>
-          </div>
-        </nav>
+
         <div id="sidebar" className={this.state.showSidebar? 'show' : 'hide'}>
 <div>
             <Link href="/" legacyBehavior>
